dea43a6046335ee42adb0cdc128ba74440e3a854
HanaWyle <hana.sherif@wyleglobal.ccom>
2025-12-19 10:41:23 +0400

    1)  Unifed navigation 2) Fixed laeblings in navigation 3) Scroll to Contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { HeroSection } from './components/HeroSection';
 import { CloneWithConscience } from './components/CloneWithConscience';
-import { FragmentedReality } from './components/FragmentedReality';
+import { GradientWave } from './components/GradientWave';
 import { ContactSection } from './components/ContactSection';
 import { MapSection } from './components/MapSection';
 import { Footer } from './components/Footer';
@@ -9,21 +9,47 @@ import { FloatingUISection } from './components/FloatingUISection';
 import { About } from './components/About';
 import { Services } from './components/Services';
 import { Contact } from './components/Contact';
-import { ProblemSection } from './components/ProblemSection';
+import { useEffect } from 'react';
 
-function HomePage() {
+
+  
+  function HomePage() {
+  const location = useLocation();
+
+  useEffect(() => {
+   if (location.state?.scrollToContact) {
+      const contactSection = document.getElementById('contact');
+      if (contactSection) {
+        contactSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
+
   return (
     <div className="relative w-full" style={{ background: '#000000' }}>
-      {/* HERO SECTION */}
+      {/* HERO SECTION
       <HeroSection />
 
-      {/* ADDITIONAL SECTIONS */}
+      {/* ADDITIONAL SECTIONS *
       <FloatingUISection />
-      <ProblemSection/>
       <CloneWithConscience />
-      <FragmentedReality />
-      {/* <GradientWave /> */}
-      <ContactSection />
+      <GradientWave />
+      <div id="contact">
+        <ContactSection />
+      </div>
+      <MapSection />
+      <Footer />
+    </div> */}
+    {/* <div className="relative w-full" style={{ background: '#000000' }}> */}
+      <HeroSection />
+      <FloatingUISection />
+      <CloneWithConscience />
+      <GradientWave />
+
+      <div id="contact">
+        <ContactSection />
+      </div>
+
       <MapSection />
       <Footer />
     </div>
